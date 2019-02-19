aef140a62f7604367fc47c23aa49b042ab2e790a
KartikPatelOfficial <patelkartik1910@gmail.com>
2019-02-20 00:52:38 +0530

    initial commit

--- src/router.js
+++ src/router.js
@@ -1,17 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Login from './views/Login'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     },
     {
       path: '/about',
@@ -19,8 +24,8 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: function () {
+        return import( /* webpackChunkName: "about" */ './views/About.vue')
       }
     }
   ]
---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <input v-model="email" placeholder="Enter your email">
+    <input v-model="password" placeholder="Enter our password">
+      <b-button variant="outline-primary">Button</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
